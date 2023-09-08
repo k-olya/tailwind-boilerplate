0aac0e5362cfa0fd82bfba337d4e17e51a95fe63
Николай Чистяков <cats@feel.so>
2023-09-08 06:58:07 +0300

    [=] initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tailwind + ES6 boilerplate
+
+Uses webpack for bundling & hmr
+
+Available scripts:
+
+start dev server:
+
+```
+npm start
+```
+
+generate production build:
+
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
